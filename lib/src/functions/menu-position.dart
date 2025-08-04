b0aeaf73eddb8dc6d4bc324edce1b3285ff5eb23
jfperon <joao.perondini@altforce.com.br>
2025-08-04 11:12:55 -0300

    2.1.1

--- lib/src/functions/menu-position.dart
+++ lib/src/functions/menu-position.dart
@@ -1,57 +1,59 @@
 part of modern_form_popupmenu;
 
 RelativeRect modernFormPopupMenuPosition(
-    BuildContext context, ModernFormPopupMenuLocation? menuLocation) {
+  BuildContext context,
+  ModernFormPopupMenuLocation? menuLocation,
+) {
   if (menuLocation == null) {
     menuLocation = ModernFormPopupMenuLocation.BottomLeft;
   }
   final RenderBox? bar = context.findRenderObject() as RenderBox?;
   final RenderBox? overlay =
-      Overlay.of(context)!.context.findRenderObject() as RenderBox?;
+      Overlay.of(context).context.findRenderObject() as RenderBox?;
   switch (menuLocation) {
     case ModernFormPopupMenuLocation.BottomLeft:
       return RelativeRect.fromRect(
-          Rect.fromPoints(
-            bar!.localToGlobal(bar.size.bottomLeft(Offset.zero),
-                ancestor: overlay),
-            bar.localToGlobal(bar.size.bottomLeft(Offset.zero),
-                ancestor: overlay),
+        Rect.fromPoints(
+          bar!.localToGlobal(
+            bar.size.bottomLeft(Offset.zero),
+            ancestor: overlay,
           ),
-          Offset.zero & overlay!.size);
+          bar.localToGlobal(
+            bar.size.bottomLeft(Offset.zero),
+            ancestor: overlay,
+          ),
+        ),
+        Offset.zero & overlay!.size,
+      );
     case ModernFormPopupMenuLocation.BottomRight:
       return RelativeRect.fromRect(
-          Rect.fromPoints(
-            bar!.localToGlobal(bar.size.bottomRight(Offset.zero),
-                ancestor: overlay),
-            bar.localToGlobal(bar.size.bottomRight(Offset.zero),
-                ancestor: overlay),
+        Rect.fromPoints(
+          bar!.localToGlobal(
+            bar.size.bottomRight(Offset.zero),
+            ancestor: overlay,
           ),
-          Offset.zero & overlay!.size);
+          bar.localToGlobal(
+            bar.size.bottomRight(Offset.zero),
+            ancestor: overlay,
+          ),
+        ),
+        Offset.zero & overlay!.size,
+      );
     case ModernFormPopupMenuLocation.TopLeft:
       return RelativeRect.fromRect(
-          Rect.fromPoints(
-            bar!.localToGlobal(bar.size.topLeft(Offset.zero),
-                ancestor: overlay),
-            bar.localToGlobal(bar.size.topLeft(Offset.zero), ancestor: overlay),
-          ),
-          Offset.zero & overlay!.size);
+        Rect.fromPoints(
+          bar!.localToGlobal(bar.size.topLeft(Offset.zero), ancestor: overlay),
+          bar.localToGlobal(bar.size.topLeft(Offset.zero), ancestor: overlay),
+        ),
+        Offset.zero & overlay!.size,
+      );
     case ModernFormPopupMenuLocation.TopRight:
       return RelativeRect.fromRect(
-          Rect.fromPoints(
-            bar!.localToGlobal(bar.size.topRight(Offset.zero),
-                ancestor: overlay),
-            bar.localToGlobal(bar.size.topRight(Offset.zero),
-                ancestor: overlay),
-          ),
-          Offset.zero & overlay!.size);
-    default:
-      return RelativeRect.fromRect(
-          Rect.fromPoints(
-            bar!.localToGlobal(bar.size.bottomLeft(Offset.zero),
-                ancestor: overlay),
-            bar.localToGlobal(bar.size.bottomLeft(Offset.zero),
-                ancestor: overlay),
-          ),
-          Offset.zero & overlay!.size);
+        Rect.fromPoints(
+          bar!.localToGlobal(bar.size.topRight(Offset.zero), ancestor: overlay),
+          bar.localToGlobal(bar.size.topRight(Offset.zero), ancestor: overlay),
+        ),
+        Offset.zero & overlay!.size,
+      );
   }
 }
